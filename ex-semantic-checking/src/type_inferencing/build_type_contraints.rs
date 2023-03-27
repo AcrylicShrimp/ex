89db03c166f793aab933be60c21e52ba940b05d6
AcrylicShrimp <led789zxpp@naver.com>
2023-03-27 19:47:37 +0900

    fix: correct type constraints for var decls.

--- ex-semantic-checking/src/type_inferencing/build_type_contraints.rs
+++ ex-semantic-checking/src/type_inferencing/build_type_contraints.rs
@@ -233,8 +233,8 @@ fn build_type_contraint_table_stmt_block(
                         &let_assignment.expression,
                     );
                     contraint_table.new_constraint(TypeConstraint::subtype(
-                        type_var,
-                        TypeConstraintTarget::variable(expr_type_var),
+                        expr_type_var,
+                        TypeConstraintTarget::variable(type_var),
                     ));
                 }
             }
